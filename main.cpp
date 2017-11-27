8f149b58f46f0b8d6075f9c9c7298071e1725d73
fabiansch <stahlschiff@web.de>
2017-11-27 21:38:48 +0100

    cleanup main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,49 @@
 #include "Header.h"
 #include <iostream>
 
-#include "Interface.h"
-#include "Receiver.h"
-#include "Parameter.h"
-
 #include "HardwareLayer.h"
 #include "LogicLayer.h"
 #include "Test.h"
-#include "Menu.h"
-
-
-#include "HeightSensor.h"
-
-#include "ItemBuffer.h"
 
 using namespace std;
 
+void evaluateIfCb_1();
+
 int main(int argc, char *argv[])
 {
-	cout << "Starting Sortingmachine ..." << endl;
-
-	LOG_SCOPE;
 	LOG_SET_LEVEL(DEBUG);
+	LOG_SCOPE;
+	cout << "========================= Start Sortingmachine ============================" << endl;
+
 
 	bool restart = false;
-
 	do {
+		evaluateIfCb_1();
 
-		//============== ASSIGNMENT OF CONVEYOR BELT ROLE ==============================
-		cb_first = CB_FIRST;
-		cb_all = CB_ALL;
-
-		string textInput;
-		do {
-			cout << "Are you conveyor belt 1? (yes/no):" << endl;
-			cin >> textInput;
-		} while (!(textInput.compare("yes") xor textInput.compare("no")));
-
-		if (!textInput.compare("yes")) {
-			cb_this = cb_first;
-		}
-		cin.get(); // get rid of extra return
-
-		//===================== INITIALISATION ==========================================
 		hardwareLayer::HardwareLayer hal;
 		logicLayer::LogicLayer loLay(hal);
-		restart = loLay.getMenu().isRestart();
 
-		//===================== COMPUTATION OF INPUT ==========================================
+		restart = loLay.getMenu().isRestart();
 	} while (restart);
 
 	cout << "===================== Shutting down Sortingmachine =========================" << endl;
 
 	return EXIT_SUCCESS;
 
+}
+
+
+void evaluateIfCb_1() {
+	//============== ASSIGNMENT OF CONVEYOR BELT ROLE ==============================
+	cb_first = CB_FIRST;
+	cb_all = CB_ALL;
+	string textInput;
+	do {
+		cout << "Are you conveyor belt 1? (yes/no):" << endl;
+		cin >> textInput;
+	} while (!(textInput.compare("yes") ^ textInput.compare("no")));
+	if (!textInput.compare("yes")) {
+		cb_this = cb_first;
+	}
+	cin.get(); // get rid of extra return
 }
