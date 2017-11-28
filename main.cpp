d95bb79b62c5b2eea69669e29d1eaa21eda57bc9
robert <robert.palm@haw-hamburg.de>
2017-11-28 15:17:09 +0100

    started working on test routine

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@
 using namespace std;
 
 void evaluateIfCb_1();
+void printStartMessage();
 
 int main(int argc, char *argv[])
 {
 	LOG_SET_LEVEL(DEBUG);
 	LOG_SCOPE;
-	cout << "========================= Start Sortingmachine ============================" << endl;
+	printStartMessage();
+//	cout << "========================= Start Sortingmachine ============================" << endl;
 
 
 	bool restart = false;
@@ -46,4 +48,19 @@ void evaluateIfCb_1() {
 		cb_this = cb_first;
 	}
 	cin.get(); // get rid of extra return
+}
+
+void printStartMessage() {
+			cout<< "		  ____                                           _              _ " <<endl;
+			cout<< "		 / ___|___  _ ____   _____ _   _  ___  _ __     / \\   _ __   __| |" <<endl;
+			cout<< "		| |   / _ \\| '_ \\ \\ / / _ \\ | | |/ _ \\| '__|   / _ \\ | '_ \\ / _` |" <<endl;
+			cout<< "		| |__| (_) | | | \\ V /  __/ |_| | (_) | |     / ___ \\| | | | (_| |" <<endl;
+			cout<< "		 \\____\\___/|_| |_|\\_/ \\___|\\__, |\\___/|_|    /_/   \\_\\_| |_|\\__,_|" <<endl;
+			cout<< "		                           |___/                                  " <<endl;
+			cout<< "		 ____             _   _               ____            _                 " <<endl;
+			cout<< "		/ ___|  ___  _ __| |_(_)_ __   __ _  / ___| _   _ ___| |_ ___ _ __ ___  " <<endl;
+			cout<< "		\\___ \\ / _ \\| '__| __| | '_ \\ / _` | \\___ \\| | | / __| __/ _ \\ '_ ` _ \\ " <<endl;
+			cout<< "		 ___) | (_) | |  | |_| | | | | (_| |  ___) | |_| \\__ \\ ||  __/ | | | | |" <<endl;
+			cout<< "		|____/ \\___/|_|   \\__|_|_| |_|\\__, | |____/ \\__, |___/\\__\\___|_| |_| |_|" <<endl;
+			cout<< "		                              |___/         |___/                       " <<endl;
 }
--- src/logicLayer/Menu.cpp
+++ src/logicLayer/Menu.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "Menu.h"
+#include "Logger.h"
+#include "Parameter.h"
 
 namespace logicLayer {
 
@@ -16,20 +18,49 @@ Menu::Menu(Channel<Signal>& controller)
 
 void Menu::computeInput(){
 	string textInput;
+	cout << "Please enter command. Type \"help\" for options.\n" << endl;
+
 	while(true){
 		cin >> textInput;
 		cin.get();
 
 		if(!textInput.compare("test")) {
-			controller_ << Signalname::TEST;
-		}
+			Logger& testLogger = Logger::getLogger();
+			cout << "\nStarts test. \nSome data is needed for logging. \n Enter name:\n" << endl;
+			cin >> textInput;
+			cin.get();
+			testLogger.logTest("Name:\t"+textInput);
 
-		if(!textInput.compare("normal")) {
-			controller_ << Signalname::RUN;
+			cout << "\nEnter code version:\n" << endl;
+			cin >> textInput;
+			cin.get();
+			testLogger.logTest("Version:\t"+textInput);
+
+			int val = (int) cb_available;
+			val++;
+			int cbCount = 0;
+			while (val > 1) {
+				val >>= 1;
+				cbCount++;
+			}
+			testLogger.logTest("Anzahl:\t"+cbCount);
+			cout<<"ES WURDEN "<<cbCount<<" CONVEYOR BELTS ERKANNT ================"<<endl;
+
+
+			cout << "\nEnter unit number of the tested festo machine:\n" << endl;
+			cin >> textInput;
+			cin.get();
+			testLogger.logTest("Name:\t"+textInput);
+
+			controller_ << Signalname::TEST;
 		}
 
 		if(!textInput.compare("calibration")) {
 			controller_ << Signalname::CALIBRATION;
+		}
+
+		if(!textInput.compare("run")) {
+			controller_ << Signalname::RUN;
 		}
 
 		if (!(textInput.compare("stop"))){
@@ -42,6 +73,17 @@ void Menu::computeInput(){
 
 		if (!(textInput.compare("restart"))){
 			restart = true;
+			return;
+		}
+		if (!(textInput.compare("help"))){
+			cout<< "You have following options:" <<endl;
+			cout<< "\ntest\t\tStarts hardware tests. Follow the given instructions." <<endl;
+			cout<< "\ncalibration\tStarts calibration. Follow the given instructions." <<endl;
+			cout<< "\nrun\t\tStarts normal running mode. " <<endl;
+			cout<< "\nstop\t\tStops current activity. You can then enter a new command." <<endl;
+			cout<< "\nshutdown\tShut down system." <<endl;
+			cout<< "\nrestart\t\tRestart System." <<endl;
+			cout<< "\nhelp\t\tShows this help text.\n" <<endl;
 		}
 	}
 }
