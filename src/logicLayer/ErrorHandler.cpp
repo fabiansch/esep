4a270f978d1e79effdabf687cc5658737aabe57f
fabiansch <stahlschiff@web.de>
2017-12-01 19:32:19 +0100

    try bugfix e stop counter

--- src/logicLayer/ErrorHandler.cpp
+++ src/logicLayer/ErrorHandler.cpp
@@ -31,15 +31,21 @@ void ErrorHandler::handle(Signal signal) {
 	switch(signal.name) {
 	case Signalname::BUTTON_E_STOP_PUSHED:
 		addPending(Signal(Signalname::BUTTON_E_STOP_PULLED));
-		hal.sendSerial(Signal(cb_this, cb_available, signal.name));
+		if(signal.sender == cb_this) {
+			hal.sendSerial(Signal(cb_this, cb_available, signal.name));
+		}
 		eStopPushedCounter++;
+		cout<<"ESTOPPPUSHED COUNTER "<<eStopPushedCounter<<endl;
 		break;
 	case Signalname::BUTTON_E_STOP_PULLED:
 		statePtr->isPending(signal);
-		hal.sendSerial(Signal(cb_this, cb_available, signal.name));
+		if(signal.sender == cb_this) {
+			hal.sendSerial(Signal(cb_this, cb_available, signal.name));
+		}
 		if(eStopPushedCounter>0) {
 			eStopPushedCounter--;
 		}
+		cout<<"ESTOPPPUSHED COUNTER "<<eStopPushedCounter<<endl;
 		break;
 	case Signalname::BUTTON_RESET_PUSHED:
 		break;
@@ -50,7 +56,6 @@ void ErrorHandler::handle(Signal signal) {
 	default:
 		statePtr->isPending(signal);
 	}
-	cout<<"ESTOPPPUSHED COUNTER "<<eStopPushedCounter<<endl;
 }
 
 } /* namespace logicLayer */
