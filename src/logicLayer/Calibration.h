ff1d3ccb8c671575509a111f5cb01d845725ed64
fabiansch <stahlschiff@web.de>
2017-12-12 12:04:36 +0100

    calibration last states

--- src/logicLayer/Calibration.h
+++ src/logicLayer/Calibration.h
@@ -303,20 +303,21 @@ private:
 					if(cb_this == cb_first){
 						hal->sendSerial(Signal(cb_this, cb_next, Signalname::CALIBRATION_SUCCESSFUL));
 						new (this) WaitingForOthers;
-					}
-					while(successful == false){
+					} else {
+						while(successful == false){
 
-					}
+						}
 					hal->sendSerial(Signal(cb_this, cb_next, Signalname::CALIBRATION_SUCCESSFUL));
-					cout << "Calibration completed." << endl;
+					cout << "Calibration completed on machine " << (int)cb_this<<endl;
 					cb_this.parameterList.showParameters();
 					new (this) IDLE;
+					}
 				}
 			};
 
 			struct WaitingForOthers: public State {
 				virtual void calibration_successful(uint8_t sender){
-					cout << "Calibration completed." << endl;
+					cout << "========== Calibration completed ===========" << endl;
 					cb_this.parameterList.showParameters();
 					new (this) IDLE;
 				}
