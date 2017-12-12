c7439ff3116c6ea1bf36f478c47ae89945d4b91e
fabiansch <stahlschiff@web.de>
2017-12-12 11:03:26 +0100

    try bugfix

--- src/logicLayer/Item.cpp
+++ src/logicLayer/Item.cpp
@@ -316,7 +316,7 @@ void Item::onOutputAction(hardwareLayer::HardwareLayer* hal, Item* item, ErrorHa
 				hal->motorStop();
 			} else {
 				hal->motorStart();
-				hal->sendSerial(Signal(cb_this, cb_previous, Signalname::START_TIMERS_INPUT));
+				hal->sendSerial(Signal(cb_this, cb_next, Signalname::START_TIMERS_INPUT));
 			}
 		}
 
