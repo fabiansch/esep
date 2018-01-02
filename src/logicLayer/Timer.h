c15d3944558c54c364e92891fc07ebe7a73a819c
fabian <stahlschiff@web.de>
2018-01-02 07:26:17 +0100

    bugfix timerbug - now all transitions between slow, fast, started and stopped are considered

--- src/logicLayer/Timer.h
+++ src/logicLayer/Timer.h
@@ -67,10 +67,11 @@ public:
 
 private:
 	Speed speed;
+	bool stopped;
 	void killTimer(Signalname);
 	//void setTimerEvent(Signalname,unsigned int,bool);
 	//void setTimerEvent(Signalname,std::chrono::steady_clock::duration,bool);
-	void setModifiedTimerEvent(TimerEvent old, bool start,std::chrono::steady_clock::time_point);
+	void setModifiedTimerEvent(TimerEvent old, std::chrono::steady_clock::time_point);
 	void initialize();
 	TimerEvent timer_events[SIZE];
 	void checkIfAvailableSpace();
